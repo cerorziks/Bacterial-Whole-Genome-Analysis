38c17d2a9e5ce658e466c2ac1c0ff4c93a4bf493
Tshering  Dorji <130952515+cerorziks@users.noreply.github.com>
2025-07-25 11:32:24 +0600

    Update assembly_spades.sh

--- assembly_spades.sh
+++ assembly_spades.sh
@@ -31,7 +31,6 @@ for r1 in ${input_dir}/*_R1_trimmed.fastq.gz; do
 
     # Run SPAdes with paired-end reads and advanced options
     spades.py --pe1-1 ${r1} --pe1-2 ${r2} \
-              --isolate \
               --careful \
               --checkpoints all \
               -o ${output_dir}/${base}_spades \
